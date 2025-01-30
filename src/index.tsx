397f5da635f67da663ef9d691cbe7a3bd836612f
FurryR <awathefox@gmail.com>
2025-01-30 04:33:56 +0000

    :sparkles: feat: reply update
    Signed-off-by: FurryR <awathefox@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,34 @@ import { parse } from "node-html-parser";
 
 import {} from "koishi-plugin-markdown-to-image-service";
 
+import {} from "@koishijs/cache";
+
+declare module "@koishijs/cache" {
+  interface Tables {
+    "ask-ai": {
+      links: string[];
+    };
+  }
+}
+
 export const name = "ask-ai";
 
 export interface Config {
   baseURL: string;
   apiKey: string;
+  model: string;
   prompt?: string;
   searchURL: string;
   textMode: boolean;
   verboseOutput: boolean;
+  cacheEnabled: boolean;
+  maxAge?: number;
 }
 
 export const Config: Schema<Config> = Schema.object({
   baseURL: Schema.string().description("OpenAI Like 接口的地址。").role("url"), // .required(),
   apiKey: Schema.string().description("API 密钥。").role("secret"), //.required(),
+  model: Schema.string().description("使用的模型名称。").default("gpt-4o-mini"),
   prompt: Schema.string()
     .role("textarea", {
       rows: [10, 10],
@@ -38,6 +52,14 @@ export const Config: Schema<Config> = Schema.object({
       "是否启用更详细的输出（如显示思考过程）。此功能可能导致风控，不建议在调试以外的场合打开。"
     )
     .default(false),
+  cacheEnabled: Schema.boolean()
+    .description(
+      "是否启用缓存 (需要缓存实现支持)。这将允许用户通过回复搜索结果来获得引用链接。"
+    )
+    .default(true),
+  maxAge: Schema.number().description(
+    "缓存最大存活时间。未指定时，使用缓存表的设置。"
+  ),
 });
 
 export const inject = {
@@ -49,13 +71,30 @@ export function apply(ctx: Context, config: Config) {
     baseURL: config.baseURL,
     apiKey: config.apiKey,
   });
-  // write your plugin here
+  ctx.on("message", async (session) => {
+    const { message } = session.event;
+    if (message && message.quote) {
+      const index = parseInt(message.content);
+      if (isNaN(index)) return;
+      const quote = await ctx.cache.get("ask-ai", message.quote.id ?? "");
+      if (!quote) return;
+      const link = quote.links[index - 1];
+      if (!link) return;
+      await session.send(
+        <>
+          <quote id={message.id} />
+          {link}
+        </>
+      );
+    }
+  });
   ctx
     .command("search <prompt:text>")
     .alias("搜索", "不懂就问")
     .action(async ({ session }, prompt) => {
       let startTime = Date.now();
       let lastMessage: string[] = [];
+      ctx.logger.info(`用户 ${session.userId} 请求搜索 ${prompt}`);
       if (config.verboseOutput) {
         lastMessage = await session.send("(正在思考)");
       }
@@ -66,7 +105,7 @@ export function apply(ctx: Context, config: Config) {
         },
       ] as const;
       const chat = await ai.chat.completions.create({
-        model: "gpt-4o-mini",
+        model: config.model,
         messages,
       });
       if (config.verboseOutput) {
@@ -85,7 +124,12 @@ export function apply(ctx: Context, config: Config) {
       if (v.length === 0 || v[0].classList.contains("result--no-result")) {
         if (config.verboseOutput)
           await session.bot.deleteMessage(session.channelId, lastMessage[0]);
-        await session.send(<>没有找到相关的搜索结果。</>);
+        await session.send(
+          <>
+            <quote id={session.messageId} />
+            没有找到相关的搜索结果。
+          </>
+        );
         return;
       }
       const result = v
@@ -114,7 +158,7 @@ export function apply(ctx: Context, config: Config) {
         })
         .join("\n---\n");
       messages[0].content = `${config.prompt ?? "你是一个对话机器人。"}
-Now, please summarize all search results with the user input as the theme matching the language of the user input. If any word, sentence, opinion or specific name of the output is from the search result, please replace the word with a Markdown link to the original content IN PLACE (for example, \`A is [the first letter of the alphabet](https://example.com/source)\`. DO NOT list the links. DO NOT emit sentences like "You can obtain more information about ... at ..." or "Here is the information about ..." Avoid any opinion not from the search result. You don't need to introduce yourself or ask for user input.
+Now, please summarize all search results with the user input as the theme matching the language of the user input. If any word, phrase, sentence, opinion or specific name of the output is from the search result, please replace the word with a Markdown link to the original content IN PLACE (for example, \`[A](https://example.com/about_a) is [the first letter of the alphabet](https://example.com/source)\`. DO NOT list the links. DO NOT emit sentences like "You can obtain more information about ... at ..." or "Here is the information about ..." Avoid any opinion not from the search result. You don't need to introduce yourself or ask for user input.
 
 ${nextPrompt}
 
@@ -125,7 +169,7 @@ ${prompt}`;
         lastMessage = await session.send("(正在总结)");
       }
       const chat2 = await ai.chat.completions.create({
-        model: "gpt-4o-mini",
+        model: config.model,
         messages,
       });
       // await session.bot.deleteMessage(session.channelId, messageId[0]);
@@ -135,12 +179,17 @@ ${prompt}`;
 
       output =
         output.replace(/\[(.*?)\]\((.*?)\)/g, (_, text, link) => {
+          if (links.includes(link)) {
+            return `<u>${text}</u>[\\[${
+              links.indexOf(link) + 1
+            }\\]](mailto:blank@example.org)`;
+          }
           links.push(link);
           return `<u>${text}</u>[\\[${links.length}\\]](mailto:blank@example.org)`;
         }) + "\n\n";
 
       for (const [idx, link] of links.entries()) {
-        output += `\\[${idx + 1}\\]: ${link}\n`;
+        output += ` \\[${idx + 1}\\]: ${link}\n`;
       }
 
       if (config.verboseOutput) {
@@ -153,25 +202,39 @@ ${prompt}`;
       const usageTime = Date.now() - startTime;
 
       if (config.textMode || !ctx.markdownToImage) {
-        await session.send(
+        const messageId = await session.send(
           <>
             <quote id={session.messageId} />
             {`# 搜索结果
 ---
 > 已展示 \`${chat.choices[0].message.content}\` 的搜索结果。
 
-${output.replace(
-  /<u>(.*?)<\/u>\[\[(.*?)\]\]\(mailto:blank@example.org\)/g,
-  (_, text, index) => {
-    return `[${text}](${links[parseInt(index) - 1]})`;
-  }
-)}
+${output
+  .replace(
+    /<u>(.*?)<\/u>\[\\\[(.*?)\\\]\]\(mailto:blank@example.org\)/g,
+    (_, text, index) => {
+      return ` ${text} [${index}] `;
+    }
+  )
+  .replace(/\\\[(.*?)\\\]: (.*?)/g, (_, index, link) => {
+    return `[${index}]: ${link}`;
+  })}
 
 ---
 
 Powered by 玻狸 × 熊谷凌 (思考用时: ${usageTime}ms)`}
           </>
         );
+        if (config.cacheEnabled && ctx.cache) {
+          await ctx.cache.set(
+            "ask-ai",
+            messageId[0] ?? "",
+            {
+              links,
+            },
+            config.maxAge
+          );
+        }
       } else {
         const markdownImage = await ctx.markdownToImage
           .convertToImage(`# 搜索结果
@@ -190,12 +253,22 @@ ${output}
         if (config.verboseOutput) {
           await session.bot.deleteMessage(session.channelId, lastMessage[0]);
         }
-        await session.send(
+        const messageId = await session.send(
           <>
             <quote id={session.messageId} />
             {h.image(markdownImage, "image/png")}
           </>
         );
+        if (config.cacheEnabled && ctx.cache) {
+          await ctx.cache.set(
+            "ask-ai",
+            messageId[0] ?? "",
+            {
+              links,
+            },
+            config.maxAge
+          );
+        }
       }
     });
 }
